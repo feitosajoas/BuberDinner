3148c795d23a177d7d2444fa014b6753bf21f35f
feitosajoas <joasfeitosadelimasilva@gmail.com>
2022-08-05 19:21:43 -0300

    Rewmovendo arquivos não utrilizados

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -11,7 +11,6 @@ builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
 
-// builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ProblemDetailsFactory, BubberDinnerProblemDetailsFactory>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
